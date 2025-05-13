34d8a79741fb5e50618f7385ca7044a7488ab638
suxixo <suraj.vishwakarma.01@yandex.com>
2025-05-13 09:45:42 +0530

    Plugin-add: fzf-lua added with mappings

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -1,4 +1,4 @@
-require("nvchad.mappings")
+require "nvchad.mappings"
 
 -- add yours here
 
@@ -8,8 +8,8 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- Open Manager
-map("n", "<leader>ol", "<cmd>Lazy<CR>", {desc = "Open lazy manager"})
-map("n", "<leader>om", "<cmd>Mason<CR>", {desc = "Open mason manager"})
+map("n", "<leader>ol", "<cmd>Lazy<CR>", { desc = "Open lazy manager" })
+map("n", "<leader>om", "<cmd>Mason<CR>", { desc = "Open mason manager" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 
@@ -18,17 +18,19 @@ map("n", "<leader>E", "<cmd>NvimTreeFocus<CR>", { desc = "nvimtree focus window"
 
 -- Mini Files
 map("n", "<leader>e", function()
-	-- if not require("mini.files").close() then
-	-- 	local buffer_type = vim.bo.buftype
-	-- 	if buffer_type == "nofile" then
-	-- 		return
-	-- 	end
-	-- 	local buffer_name = vim.api.nvim_buf_get_name(0)
-	-- 	if buffer_name then
-	-- 		require("mini.files").open(buffer_name)
-	-- 	end
-	-- end
-	if not require("mini.files").close() then
-		require("mini.files").open()
-	end
+  -- if not require("mini.files").close() then
+  -- 	local buffer_type = vim.bo.buftype
+  -- 	if buffer_type == "nofile" then
+  -- 		return
+  -- 	end
+  -- 	local buffer_name = vim.api.nvim_buf_get_name(0)
+  -- 	if buffer_name then
+  -- 		require("mini.files").open(buffer_name)
+  -- 	end
+  -- end
+  if not require("mini.files").close() then
+    require("mini.files").open()
+  end
 end, { noremap = true, desc = "Open mini files" })
+
+map("n", "<leader>ff", "<cmd>FzfLua files<CR>", { desc = "Find files" })
