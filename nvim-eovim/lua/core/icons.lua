2ff7ac4c3e421dc2451cc30556e393e5e669e48b
suxixo <suraj.vishwakarma.01@yandex.com>
2025-05-18 02:30:51 +0530

    updated get_icons function to handle multiple arguments and get icons from nested table

--- nvim-eovim/lua/core/icons.lua
+++ nvim-eovim/lua/core/icons.lua
@@ -2,25 +2,45 @@ local H = {}
 H.icons = {
   notfound = '󰱞',
   finder = "",
+  folder_close = "",
+  folder_empty = "",
+  folder_empty_open = "",
+  folder_open = "",
+  folder_symlink = "",
+  folder_symlink_open = "",
+  folder_filter = "󱧋",
+  modified = "󰐖",
+  hidden = "󰰀",
+  unstaged = "✗",
+  staged = "✓",
+  unmerged = "",
+  renamed = "",
+  untracked = "󰩴",
+  deleted = "",
+  ignored = "◌",
+  indent = {
+    A1 = "▏",
+    A2 = "│",
+    A3 = "┊",
+    A4 = "╎",
+  },
 }
-H.get_icons = function (iconKey)
-  iconKey = iconKey or ''
-  if vim.fn.empty(iconKey) == 1 then
+H.get_icons = function (...)
+  if select('#', ...) == 0 then
     vim.print("Pass icon key...")
-    return iconKey
   else
-    if vim.iter(H.icons):any(function (key, _) return key == iconKey end) then
-      -- vim.print(H.icons[iconKey])
-      return H.icons[iconKey]
+    local item = vim.tbl_get(H.icons, ...)
+    if item ~= nil then
+      return item
     else
-      -- vim.print("󰱞")
-      return H.icons.notfound
+      return vim.tbl_get(H.icons, "notfound")
     end
   end
 end
 H.text_icons = {}
 
 -- vim.print(H.get_icons())
+-- vim.print(H.get_icons("indent", "A1"))
 -- vim.print(H.get_icons('finder'))
 -- vim.print(H.get_icons('finr'))
 return H
