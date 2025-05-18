fe461e891818c1b436d881ff205077c5ae7f55bf
suxixo <suraj.vishwakarma.01@yandex.com>
2025-05-18 15:16:25 +0530

    plugin-add: autopair

--- nvim-eovim/lua/plugins/editor.lua
+++ nvim-eovim/lua/plugins/editor.lua
@@ -88,6 +88,21 @@ return {
     end,
   },
   {
+    "windwp/nvim-autopairs",
+    event = { "InsertEnter" },
+    config = function()
+      local autopairs = require("nvim-autopairs")
+      autopairs.setup({
+        check_ts = true, -- enable treesitter
+        ts_config = {
+          lua = { "string" }, -- don't add pairs in lua string treesitter nodes
+          javascript = { "template_string" }, -- don't add pairs in javscript template_string treesitter nodes
+          java = false, -- don't check treesitter on java
+        },
+      })
+    end,
+  },
+  {
     "echasnovski/mini.pick",
     version = '*',
     name = "mini-pick",
