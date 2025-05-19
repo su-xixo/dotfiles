59a7436111b10e3c96256fa5d07d6a9e4b9e23b4
suxixo <suraj.vishwakarma.01@yandex.com>
2025-05-19 15:59:06 +0530

    because of global variable automatic mason tools are not installing

--- nvim-eeovim/lua/plugins/configs/mason.lua
+++ nvim-eeovim/lua/plugins/configs/mason.lua
@@ -2,10 +2,10 @@ local mason = require("mason")
 local mason_tool_installer = require("mason-tool-installer")
 local function get_tools()
   local merged_tbl = {}
-  table.insert(merged_tbl, eovim.lang.lsp)
-  table.insert(merged_tbl, eovim.lang.fmt)
-  table.insert(merged_tbl, eovim.lang.linter)
-  table.insert(merged_tbl, eovim.lang.dap)
+  table.insert(merged_tbl, require("core.my_default").lang.lsp)
+  table.insert(merged_tbl, require("core.my_default").lang.fmt)
+  table.insert(merged_tbl, require("core.my_default").lang.linter)
+  table.insert(merged_tbl, require("core.my_default").lang.dap)
   return vim.iter(merged_tbl):flatten(math.huge):totable()
 end
 
