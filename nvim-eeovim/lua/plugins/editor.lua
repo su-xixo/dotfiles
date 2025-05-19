3d036d33d3cc21f93baddc987f86e76fa35c1303
suxixo <suraj.vishwakarma.01@yandex.com>
2025-05-19 09:04:37 +0530

    added keymap for files

--- nvim-eeovim/lua/plugins/editor.lua
+++ nvim-eeovim/lua/plugins/editor.lua
@@ -3,6 +3,7 @@ return {
     "echasnovski/mini.files",
     version = "*",
     name = "mini-files",
+    keys = require("core.mappings").getmap("tree", "minifiles"),
     config = function()
       return require("plugins.configs.mini.files")
     end,
