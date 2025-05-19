1e4157e4fcb99fd422b9f5baff652f4099a81f60
suxixo <suraj.vishwakarma.01@yandex.com>
2025-05-19 07:33:52 +0530

    added getmap function to require it anywhere

--- nvim-eeovim/lua/core/mappings.lua
+++ nvim-eeovim/lua/core/mappings.lua
@@ -1,5 +1,5 @@
 local H = {}
-H.active  = {}
+H.active = {}
 
 H.active.basic = {
   { "jk", "<Esc>", mode = { "i" }, desc = "Escape insert mode" },
@@ -27,8 +27,6 @@ H.active.basic = {
 
   { "<leader>n", "<cmd>set nu!<CR>", desc = "toggle line number" },
   { "<leader>rn", "<cmd>set rnu!<CR>", desc = "toggle relative number" },
-  { "<leader>ch", "<cmd>NvCheatsheet<CR>", desc = "toggle nvcheatsheet" },
-
 }
 
 H.active.manager = {
@@ -36,14 +34,13 @@ H.active.manager = {
   { "<leader>om", "<cmd>Mason<CR>", desc = "Open mason manager" },
 }
 
-H.active.tree = {
-  { "<leader>E", "<cmd>NvimTreeToggle<CR>", desc = "nvimtree toggle window" },
-  { "<C-n>", "<cmd>NvimTreeFocus<CR>", desc = "nvimtree focus window" },
-  { "<leader>e", function ()
-    if not require("mini.files").close() then
-      require("mini.files").open()
-    end
-  end, desc = "Mini file explorer" }
+H.tree = {
+  nvimtree = {
+    { "<leader>E", "<cmd>NvimTreeToggle<CR>", desc = "nvimtree toggle window" },
+    { "<C-n>", "<cmd>NvimTreeFocus<CR>", desc = "nvimtree focus window" },
+  },
+  neotree = {},
+  minifiles = H.mini.files,
 }
 
 H.nvchad = {
@@ -68,27 +65,84 @@ H.nvchad = {
   { "<leader>rn", "<cmd>set rnu!<CR>", desc = "toggle relative number" },
   { "<leader>ch", "<cmd>NvCheatsheet<CR>", desc = "toggle nvcheatsheet" },
   -- Comment
-  { "<leader>/", "gcc", desc = "toggle comment", remap = true  },
-  { "<leader>/", "gc", desc = "toggle comment", mode = "v", remap = true  },
+  { "<leader>/", "gcc", desc = "toggle comment", remap = true },
+  { "<leader>/", "gc", desc = "toggle comment", mode = "v", remap = true },
 
   -- tabufline
   { "<leader>b", "<cmd>enew<CR>", desc = "buffer new" },
-  { "<tab>", function() require("nvchad.tabufline").next() end, desc = "buffer goto next" },
-  { "<S-tab>", function() require("nvchad.tabufline").prev() end, desc = "buffer goto prev" },
-  { "<leader>x", function() require("nvchad.tabufline").close_buffer() end, desc = "buffer close" },
-  { "<leader>th", function() require("nvchad.themes").open() end, desc = "telescope nvchad themes" },
+  {
+    "<tab>",
+    function()
+      require("nvchad.tabufline").next()
+    end,
+    desc = "buffer goto next",
+  },
+  {
+    "<S-tab>",
+    function()
+      require("nvchad.tabufline").prev()
+    end,
+    desc = "buffer goto prev",
+  },
+  {
+    "<leader>x",
+    function()
+      require("nvchad.tabufline").close_buffer()
+    end,
+    desc = "buffer close",
+  },
+  {
+    "<leader>th",
+    function()
+      require("nvchad.themes").open()
+    end,
+    desc = "telescope nvchad themes",
+  },
 
   -- terminal
   { "<C-x>", "<C-\\><C-N>", desc = "terminal escape terminal mode" },
 
   -- new terminals
-  { "<leader>h", function() require("nvchad.term").new { pos = "sp" } end, desc = "terminal new horizontal term" },
-  { "<leader>v", function() require("nvchad.term").new { pos = "vsp" } end, desc = "terminal new vertical term" },
+  {
+    "<leader>h",
+    function()
+      require("nvchad.term").new({ pos = "sp" })
+    end,
+    desc = "terminal new horizontal term",
+  },
+  {
+    "<leader>v",
+    function()
+      require("nvchad.term").new({ pos = "vsp" })
+    end,
+    desc = "terminal new vertical term",
+  },
 
   -- toggleable
-  { "<A-v>", function() require("nvchad.term").toggle { pos = "vsp", id = "vtoggleTerm" } end, mode = { "n", "t" }, desc = "terminal toggleable vertical term" },
-  { "<A-h>", function() require("nvchad.term").toggle { pos = "sp", id = "htoggleTerm" } end, mode = { "n", "t" }, desc = "terminal toggleable horizontal term" },
-  { "<A-i>", function() require("nvchad.term").toggle { pos = "float", id = "floatTerm" } end, mode = { "n", "t" }, desc = "terminal toggle floating term" },
+  {
+    "<A-v>",
+    function()
+      require("nvchad.term").toggle({ pos = "vsp", id = "vtoggleTerm" })
+    end,
+    mode = { "n", "t" },
+    desc = "terminal toggleable vertical term",
+  },
+  {
+    "<A-h>",
+    function()
+      require("nvchad.term").toggle({ pos = "sp", id = "htoggleTerm" })
+    end,
+    mode = { "n", "t" },
+    desc = "terminal toggleable horizontal term",
+  },
+  {
+    "<A-i>",
+    function()
+      require("nvchad.term").toggle({ pos = "float", id = "floatTerm" })
+    end,
+    mode = { "n", "t" },
+    desc = "terminal toggle floating term",
+  },
 }
 
 H.fzf_lua = {
@@ -97,10 +151,26 @@ H.fzf_lua = {
   { "<leader>fr", "<cmd>FzfLua resume<CR>", desc = "Resume finding" },
   { "<leader>fh", "<cmd>FzfLua helptags<CR>", desc = "Find help" },
   { "<leader>fo", "<cmd>FzfLua oldfiles<CR>", desc = "Find oldfiles" },
-  { "<leader>fd", "<cmd>FzfLua lsp_document_diagnostics<CR>", desc = "Find current diagnostic" },
-  { "<leader>fD", "<cmd>FzfLua lsp_workspace_diagnostics<CR>", desc = "Find all diagnostic" },
-  { "<leader>fg", "<cmd>FzfLua live_grep<CR>", desc = "Grep live whole project" },
-  { "<leader>/", "<cmd>FzfLua blines<CR>", desc = "Grep live in current buffer" },
+  {
+    "<leader>fd",
+    "<cmd>FzfLua lsp_document_diagnostics<CR>",
+    desc = "Find current diagnostic",
+  },
+  {
+    "<leader>fD",
+    "<cmd>FzfLua lsp_workspace_diagnostics<CR>",
+    desc = "Find all diagnostic",
+  },
+  {
+    "<leader>fg",
+    "<cmd>FzfLua live_grep<CR>",
+    desc = "Grep live whole project",
+  },
+  {
+    "<leader>/",
+    "<cmd>FzfLua blines<CR>",
+    desc = "Grep live in current buffer",
+  },
   { "<leader>fB", "<cmd>FzfLua builtin<CR>", desc = "Find buildins" },
   {
     "<leader>Fn",
@@ -128,37 +198,56 @@ H.pick = {
   { "<leader>fr", "<cmd>Pick resume<CR>", desc = "Resume finding" },
   { "<leader>fh", "<cmd>Pick help<CR>", desc = "Find help" },
   { "<leader>fo", "<cmd>Pick oldfiles<CR>", desc = "Find oldfiles" },
-  { "<leader>fd", "<cmd>Pick diagnostic scope='current'<CR>", desc = "Find current diagnostic" },
-  { "<leader>fD", "<cmd>Pick diagnostic scope='all'<CR>", desc = "Find all diagnostic" },
+  {
+    "<leader>fd",
+    "<cmd>Pick diagnostic scope='current'<CR>",
+    desc = "Find current diagnostic",
+  },
+  {
+    "<leader>fD",
+    "<cmd>Pick diagnostic scope='all'<CR>",
+    desc = "Find all diagnostic",
+  },
   { "<leader>fg", "<cmd>Pick grep_live<CR>", desc = "Grep live whole project" },
-  { "<leader>/", "<cmd>Pick buf_lines<CR>", desc = "Grep live in current buffer" },
+  {
+    "<leader>/",
+    "<cmd>Pick buf_lines<CR>",
+    desc = "Grep live in current buffer",
+  },
   { "<leader>fB", "<cmd>Pick buildins<CR>", desc = "Find buildins" },
-  { "<leader>Fn", "<cmd>Pick nvim_config<CR>", desc = "Find within neovim config" },
+  {
+    "<leader>Fn",
+    "<cmd>Pick nvim_config<CR>",
+    desc = "Find within neovim config",
+  },
   { "<leader>Fd", "<cmd>Pick dotfiles<CR>", desc = "Find within dotfiles" },
   { "<leader>Fc", "<cmd>Pick config_dir<CR>", desc = "Find within config_dir" },
 }
 
-H.trailspace = {
-  { "<leader>ts", "<cmd>lua MiniTrailspace.trim()<CR>", desc = "Trail all spaces" },
-  { "<leader>tl", "<cmd>lua MiniTrailspace.trim_last_lines()<CR>", desc = "Trail all empty lines" },
-}
-
-H.formatter = {
+H.H.formatter = {
   {
     "<leader>mp",
     function()
-      require("conform").format({ async = true, lsp_fallback = true, timeout_ms = 1000, })
+      require("conform").format({
+        async = true,
+        lsp_fallback = true,
+        timeout_ms = 1000,
+      })
     end,
     mode = { "n", "v" },
-    desc = "Format buffer"
+    desc = "Format buffer",
   },
   {
     "<C-m><C-p>",
     function()
-      require("conform").format({ async = true, lsp_fallback = true, timeout_ms = 1000, })
+      require("conform").format({
+        async = true,
+        lsp_fallback = true,
+        timeout_ms = 1000,
+      })
     end,
     mode = { "i" },
-    desc = "Format buffer"
+    desc = "Format buffer",
   },
 }
 
@@ -176,9 +265,54 @@ H.setmap = function(keyTable)
     end
   end
 end
-H.active_map = function ()
+
+H.mini = {
+  files = {
+    {
+      "<leader>e",
+      function()
+        if not require("mini.files").close() then
+          require("mini.files").open()
+        end
+      end,
+      desc = "Mini file explorer",
+    },
+  },
+  pick = {},
+  trailspace = {
+    {
+      "<leader>ts",
+      "<cmd>lua MiniTrailspace.trim()<CR>",
+      desc = "Trail all spaces",
+    },
+    {
+      "<leader>tl",
+      "<cmd>lua MiniTrailspace.trim_last_lines()<CR>",
+      desc = "Trail all empty lines",
+    },
+  },
+}
+
+H.getmap = function(...)
+  if select("#", ...) == 0 then
+    vim.print("Must pass key...")
+  else
+    local item = vim.tbl_get(H, ...)
+    if not vim.fn.empty(item) and vim.islist(item) then
+      return item
+    else
+      return "Nothing found. check Neovim 'core/mappings.lua'"
+    end
+  end
+end
+
+H.active_map = function()
   for k, _ in pairs(H.active) do
     H.setmap(H.active[k])
   end
 end
+
+vim.print(H.getmap("mini", "pick"))
+vim.print(H.getmap("nvchad"))
+
 return H
