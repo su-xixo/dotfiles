759e7186342386f0fba78fc70ea3d21e609d6e28
suxixo <suraj.vishwakarma.01@yandex.com>
2025-05-24 14:17:19 +0530

    added hipattern for alt word

--- nvim-eeovim/lua/plugins/configs/mini/hipatterns.lua
+++ nvim-eeovim/lua/plugins/configs/mini/hipatterns.lua
@@ -6,6 +6,7 @@ options.highlighters = {
   hack = { pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack" },
   todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
   note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
+  alt = { pattern = "%f[%w]()Alt()%f[%W]", group = "MiniHipatternsNote" },
 
   -- Highlight hex color strings (`#rrggbb`) using that color
   hex_color = hipatterns.gen_highlighter.hex_color(),
