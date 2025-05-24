f4f2d7387328ac295d3542b671cf215ee1de1083
suxixo <suraj.vishwakarma.01@yandex.com>
2025-05-24 13:45:30 +0530

    plugin-add: mini-git

--- nvim-eeovim/lua/plugins/git.lua
+++ nvim-eeovim/lua/plugins/git.lua
@@ -1,5 +1,27 @@
 return {
   {
+    "echasnovski/mini-git",
+    event = { "BufReadPost", "BufWritePost" },
+    version = "*",
+    config = function()
+      require("mini.git").setup({})
+    end,
+  },
+  {
+    "echasnovski/mini.diff",
+    event = { "BufReadPost", "BufWritePost" },
+    version = "*",
+    config = function()
+      require("mini.diff").setup({
+        view = {
+          style = "sign",
+          signs = { add = "▒", change = "▒", delete = "▒" },
+          priority = 199,
+        },
+      })
+    end,
+  },
+  {
     "kdheepak/lazygit.nvim",
     dependencies = {
       "nvim-lua/plenary.nvim",
