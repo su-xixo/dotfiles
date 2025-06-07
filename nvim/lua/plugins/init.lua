6fdfc0be28897ca5b64b8866c26c1216645206de
suxixo <suraj.vishwakarma.01@yandex.com>
2025-06-07 22:12:02 +0530

    enabled format on save

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -2,6 +2,7 @@ return {
   {
     "stevearc/conform.nvim",
     -- event = 'BufWritePre', -- uncomment for format on save
+    event = 'BufWritePre', -- uncomment for format on save
     opts = require "configs.conform",
   },
 
