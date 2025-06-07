fee7557655b168faf77f0880769d9f4a6478f661
suxixo <suraj.vishwakarma.01@yandex.com>
2025-06-07 12:12:35 +0530

    updated basic chadrc

--- nvim/lua/chadrc.lua
+++ nvim/lua/chadrc.lua
@@ -1,24 +1,43 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
--- Please read that file to know all available options :( 
+-- Please read that file to know all available options :(
 
 ---@type ChadrcConfig
 local M = {}
 
 M.base46 = {
-	theme = "onedark",
+  theme = "kanagawa",
+  integrations = {
+    "blankline",
+    "cmp",
+    "codeactionmenu",
+    "dap",
+    "devicons",
+    "hop",
+    "lsp",
+    "markview",
+    "mason",
+    "neogit",
+    "notify",
+    "nvimtree",
+    "rainbowdelimiters",
+    "semantic_tokens",
+    "todo",
+    "whichkey",
+  },
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+  -- hl_override = {
+  -- 	Comment = { italic = true },
+  -- 	["@comment"] = { italic = true },
+  -- },
 }
 
--- M.nvdash = { load_on_startup = true }
--- M.ui = {
---       tabufline = {
---          lazyload = false
---      }
---}
+M.nvdash = { load_on_startup = true }
+M.ui = {
+  telescope = { style = "bordered" },
+  tabufline = {
+    lazyload = false,
+  },
+}
 
 return M
