a1d6a1022917a100ca3a89d3b476f706fcf86b03
suxixo <suraj.vishwakarma.01@yandex.com>
2025-06-07 16:19:24 +0530

    nvdash new mapping added for lazy and manager, quite, session restore

--- nvim/lua/chadrc.lua
+++ nvim/lua/chadrc.lua
@@ -32,7 +32,51 @@ M.base46 = {
   -- },
 }
 
-M.nvdash = { load_on_startup = true }
+M.nvdash = {
+  load_on_startup = true,
+  header = {
+    "                            ",
+    "     ▄▄         ▄ ▄▄▄▄▄▄▄   ",
+    "   ▄▀███▄     ▄██ █████▀    ",
+    "   ██▄▀███▄   ███           ",
+    "   ███  ▀███▄ ███           ",
+    "   ███    ▀██ ███           ",
+    "   ███      ▀ ███           ",
+    "   ▀██ █████▄▀█▀▄██████▄    ",
+    "     ▀ ▀▀▀▀▀▀▀ ▀▀▀▀▀▀▀▀▀▀   ",
+    "                            ",
+    "     Powered By  eovim    ",
+    "                            ",
+  },
+  buttons = {
+    --stylua: ignore start
+    { txt = "  Find File", keys = "ff", cmd = "Telescope find_files" },
+    { txt = "  Recent Files", keys = "fo", cmd = "Telescope oldfiles" },
+    { txt = "󰈭  Find Word", keys = "fw", cmd = "Telescope live_grep" },
+    { txt = "󱥚  Themes", keys = "th", cmd = ":lua require('nvchad.themes').open()" },
+    { txt = "  Mappings", keys = "ch", cmd = "NvCheatsheet" },
+    { txt = "  Restore Session", keys = "sr", cmd = ":lua require('persistence').load()" },
+    { txt = "󱁤  Mason", keys = "om", cmd = "Mason" },
+    { txt = "󰒲  Lazy", keys = "ol", cmd = "Lazy" },
+    { txt = "  Quit", keys = "q", cmd = ":lua vim.api.nvim_command('qa')" },
+    -- { txt = "  Quit", keys = "q", cmd = ":lua vim.cmd('q')" },
+
+    { txt = "─", hl = "NvDashFooter", no_gap = true, rep = true },
+
+    {
+      txt = function()
+        local stats = require("lazy").stats()
+        local ms = math.floor(stats.startuptime) .. " ms"
+        return "  Loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms
+      end,
+      hl = "NvDashFooter",
+      no_gap = true,
+    },
+
+    { txt = "─", hl = "NvDashFooter", no_gap = true, rep = true },
+    -- stylua: ignore end
+  },
+}
 M.ui = {
   telescope = { style = "bordered" },
   -- tabufline = {
