7d483c26b4aaa9553664a182562a53483e5621e1
suxixo <suraj.vishwakarma.01@yandex.com>
2025-06-08 17:10:24 +0530

    plugin-add: toggle-bool

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -64,6 +64,40 @@ return {
       { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
     },
   },
+  {
+    "gerazov/toggle-bool.nvim",
+    keys = {
+      { "<leader>tt" },
+    },
+    config = function()
+      require("toggle-bool").setup {
+        mapping = "<leader>tt",
+        additional_toggles = {
+          left = "right",
+          Yes = "No",
+          On = "Off",
+          ["0"] = "1",
+          Enable = "Disable",
+          Enabled = "Disabled",
+          First = "Last",
+          Before = "After",
+          Persistent = "Ephemeral",
+          Internal = "External",
+          Start = "Stop",
+          In = "Out",
+          Open = "Close",
+          Enter = "Exit",
+          Lock = "Unlock",
+          Connect = "Disconnect",
+          Mount = "Dismount",
+          Import = "Export",
+          Ingress = "Egress",
+          Allow = "Deny",
+          All = "None",
+        },
+      }
+    end,
+  },
 
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
