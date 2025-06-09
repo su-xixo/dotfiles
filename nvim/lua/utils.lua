5c3415db81b45fbaa2be1acf397ee5c068d0284a
suxixo <suraj.vishwakarma.01@yandex.com>
2025-06-09 20:58:54 +0530

    function to get plugin options

--- nvim/lua/utils.lua
+++ nvim/lua/utils.lua
@@ -8,4 +8,12 @@ M.has = function(name)
   return M.get_plugin(name) ~= nil
 end
 
+M.opts = function(name)
+  local plugin = M.get_plugin(name)
+  if plugin == nil then
+    return {}
+  end
+  local Plugin = require "lazy.core.plugin"
+  return Plugin.values(plugin, "opts", false)
+end
 return M
